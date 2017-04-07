7864884159b453196d89750cee6ac9ecc979c898
baybird <bayareabird@gmail.com>
2017-04-06 21:05:24 -0700

    solution for add two numbers

--- addTwoNumbers.py
+++ addTwoNumbers.py
@@ -1,7 +1,4 @@
-# Filename      : Add two numbers
-# Author        : Robert Tang
-# Created       : 4/6/2017
-# Last Modified :
+# Problem       : Add two numbers
 # Description   : You are given two non-empty linked lists representing two non-negative
 #                 integers. The digits are stored in reverse order and each of their nodes
 #                 contain a single digit. Add the two numbers and return it as a linked list.
@@ -10,6 +7,8 @@
 # Input         : (2 -> 4 -> 3) + (5 -> 6 -> 4)
 # Output        : 7 -> 0 -> 8
 # Python Version: 2.7
+# Author        : Robert Tang
+# Created       : 4/6/2017
 
 # Definition for singly-linked list.
 class ListNode(object):
